de4923d988f3102617fd8ece5dc0a6383a15f1da
denis1 <example@gmail.com>
2023-01-07 23:54:57 +0300

    1 сохранение

---
+++ script.js
@@ -0,0 +1,37 @@
+// Урок 14 Интерполяция (ES6)
+
+// const category = 'toys';
+// console.log(`http://somuerl.com/${category}/5`)
+
+// const user = "Денис";
+// alert(`Привет, ${user}`);
+
+
+// Урок 13 Простое общение с пользователем.
+
+alert("Задача выполнена!");
+ const result = confirm("hou are you");
+ console.log(result);
+const answer = +prompt("Вам есть 18", "18");
+console.log(answer + 5);
+const answers = [];
+answers[0] = prompt('ваще имя?', '');
+answers[1] = prompt('ваша фамилия?', '');
+answers[2] = prompt('ваш возраст?', '');
+console.log(typeof(answers));
+console.log(typeof(null));
+
+
+// урок 15 Операторы в js
+
+console.log ('arr' + " - object"); 
+console.log (4 + + "5"); 
+
+let incr = 10;
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr)
+console.log(decr)
